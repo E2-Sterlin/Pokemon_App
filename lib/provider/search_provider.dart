923b91915cf05b39fd6f9bffa0506041b6d02a2e
E2-Sterlin <120548459+E2-Sterlin@users.noreply.github.com>
2024-08-23 12:17:38 +0530

    removing search provider

--- lib/provider/search_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-class SearchProvider extends StateNotifier<Map<String, dynamic>> {
-  SearchProvider(super._state, {required this.number});
-  final String number;
-}
